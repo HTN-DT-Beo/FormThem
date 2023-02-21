5840e15f7e1398a8c4a1ef623e49327061aff666
Huỳnh Thiện Nhân <159.thiennhan@gmail.com>
2023-02-21 14:17:09 +0700

    Updated commit

--- FormThem.Designer.cs
+++ FormThem.Designer.cs
@@ -35,7 +35,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.ForeColor = System.Drawing.Color.Black;
             this.label1.Location = new System.Drawing.Point(214, 209);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(322, 29);
